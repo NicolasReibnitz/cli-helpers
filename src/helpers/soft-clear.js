4928f0513524022f739278c3411943ad4e245c72
Nicolas Reibnitz <nicolas.reibnitz@simpleshow.com>
2021-09-03 17:41:33 -0400

    Added 'prettier-config-interactive'

--- src/helpers/soft-clear.js
+++ src/helpers/soft-clear.js
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
 
 /**
- *
+ * Clears the terminal in the same way that the 'clear' command does (not erasing the history).
  */
 function softClear() {
 	execSync('clear', {
